30d3aa88dbc3f0a8707b08e8b656adaca7411419
Mikhail <mikhailyuwankho@gmail.com>
2023-09-23 19:51:54 +0700

    adding .env and change port

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import helmet from "helmet";
+import express from "express"
+import morgan from "morgan"
+import cors from "cors"
+import helmet from "helmet"
 
-import { connetionMysql } from "./src/config/db.js";
+import { connetionMysql } from "./src/config/db.js"
 
-import userRoutes from "./src/routes/user.js";
-import eventRoutes from "./src/routes/event.js";
-import orderRoutes from "./src/routes/order.js";
-import reviewRoutes from "./src/routes/review.js";
+import userRoutes from "./src/routes/user.js"
+import eventRoutes from "./src/routes/event.js"
+import orderRoutes from "./src/routes/order.js"
+import reviewRoutes from "./src/routes/review.js"
 
-import errorHandler from "./src/utils/error-handler.js";
-import notFound from "./src/utils/not-found.js";
+import errorHandler from "./src/utils/error-handler.js"
+import notFound from "./src/utils/not-found.js"
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 8000
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(morgan("dev"));
-app.use(cors());
-app.use(helmet());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(morgan("dev"))
+app.use(cors())
+app.use(helmet())
 
-connetionMysql();
+connetionMysql()
 
-app.use("api/user", userRoutes);
-app.use("api/event", eventRoutes);
-app.use("api/order", orderRoutes);
-app.use("api/review", reviewRoutes);
+app.use("api/user", userRoutes)
+app.use("api/event", eventRoutes)
+app.use("api/order", orderRoutes)
+app.use("api/review", reviewRoutes)
 
-app.use(errorHandler);
-app.use(notFound);
+app.use(errorHandler)
+app.use(notFound)
 
 app.listen(port, () => {
-  console.log(`Server running on PORT: ${port}`);
-});
+  console.log(`Server running on PORT: ${port}`)
+})
